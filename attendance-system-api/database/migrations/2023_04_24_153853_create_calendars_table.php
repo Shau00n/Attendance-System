16e0a7ed2276a5541baef112f4a548c7f7018045
Shau00n <flankeeeer7@gmail.com>
2023-04-25 00:46:12 +0900

    マイグレーションファイル(calendars)の編集

--- attendance-system-api/database/migrations/2023_04_24_153853_create_calendars_table.php
+++ attendance-system-api/database/migrations/2023_04_24_153853_create_calendars_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('calendars', function (Blueprint $table) {
-            $table->id();
+            $table->date('date')->primary();
+            $table->boolean('is_holiday')->default(false);
+            $table->string('holiday_name')->nullable();
             $table->timestamps();
         });
     }
