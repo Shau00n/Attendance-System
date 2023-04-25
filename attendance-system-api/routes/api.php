9232276847964211b522e5ef739b23bd3ea9b8dd
Shau00n <flankeeeer7@gmail.com>
2023-04-26 01:06:39 +0900

    attendanceとcalendarのルーティングを仮の状態だが設定した。

--- attendance-system-api/routes/api.php
+++ attendance-system-api/routes/api.php
@@ -23,12 +23,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 // User関連のエンドポイント
-Route::get('/users', 'UserController@index');
-Route::get('/users/{id}', 'UserController@show');
-Route::post('/users', 'UserController@store');
-Route::put('/users/{id}', 'UserController@update');
-Route::delete('/users/{id}', 'UserController@destroy');
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/users/{id}', [UserController::class, 'show']);
+Route::post('/users', [UserController::class, 'store']);
+Route::put('/users/{id}', [UserController::class, 'update']);
+Route::delete('/users/{id}', [UserController::class, 'destroy']);
 
 // Attendance関連のエンドポイント
+Route::get('/attendances', [AttendanceController::class, 'index']);
+Route::get('/attendances/{id}', [AttendanceController::class, 'show']);
+Route::post('/attendances', [AttendanceController::class, 'store']);
+Route::put('/attendances/{id}', [AttendanceController::class, 'update']);
+Route::delete('/attendances/{id}', [AttendanceController::class, 'destroy']);
 
 // Calendar関連のエンドポイント
+Route::get('/calendars', [CalendarController::class, 'index']);
+Route::get('/calendars/{id}', [CalendarController::class, 'show']);
+Route::post('/calendars', [CalendarController::class, 'store']);
+Route::put('/calendars/{id}', [CalendarController::class, 'update']);
+Route::delete('/calendars/{id}', [CalendarController::class, 'destroy']);
