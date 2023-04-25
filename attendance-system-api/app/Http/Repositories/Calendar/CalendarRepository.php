6d2c5cd228ba45ea3a57074172f2e372353d88b4
Shau00n <flankeeeer7@gmail.com>
2023-04-26 01:46:03 +0900

    カレンダーリポジトリの作成

---
+++ attendance-system-api/app/Http/Repositories/Calendar/CalendarRepository.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Repositories\Calendar;
+
+use App\Models\Calendar;
+
+class CalendarRepository implements CalendarRepositoryInterface
+{
+    protected $calendar;
+
+    public function __construct(Calendar $calendar)
+    {
+        $this->calendar = $calendar;
+    }
+
+    public function all()
+    {
+        return $this->calendar->all();
+    }
+
+    public function findById($id)
+    {
+        return $this->calendar->findOrFail($id);
+    }
+
+    public function create(array $data)
+    {
+        return $this->calendar->create($data);
+    }
+
+    public function update(array $data, $id)
+    {
+        $calendar = $this->calendar->findOrFail($id);
+        $calendar->update($data);
+
+        return $calendar;
+    }
+
+    public function delete($id)
+    {
+        $calendar = $this->calendar->findOrFail($id);
+        $calendar->delete();
+
+        return true;
+    }
+}
