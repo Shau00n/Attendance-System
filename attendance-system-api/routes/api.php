0122339a782ef5d694377dc75fdebced1c72d8ed
Shau00n <flankeeeer7@gmail.com>
2023-04-26 00:41:18 +0900

    Userコントローラ周りのエンドポイント作成

--- attendance-system-api/routes/api.php
+++ attendance-system-api/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\CalendarController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// User関連のエンドポイント
+Route::get('/users', 'UserController@index');
+Route::post('/users', 'UserController@store');
+Route::get('/users/{id}', 'UserController@show');
+Route::put('/users/{id}', 'UserController@update');
+Route::delete('/users/{id}', 'UserController@destroy');
+
+// Attendance関連のエンドポイント
+
+// Calendar関連のエンドポイント
