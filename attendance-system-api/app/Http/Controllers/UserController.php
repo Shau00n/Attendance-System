ab741d44d7ce4b47906ce1d5807aec7e1219298b
Shau00n <flankeeeer7@gmail.com>
2023-04-26 00:59:35 +0900

    ディレクトリを修正（\Userを追加）

--- attendance-system-api/app/Http/Controllers/UserController.php
+++ attendance-system-api/app/Http/Controllers/UserController.php
@@ -2,7 +2,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Repositories\UserRepositoryInterface;
+use App\Repositories\User\UserRepositoryInterface;
 
 class UserController extends Controller
 {
--- attendance-system-api/app/Http/Repositories/User/UserRepositoryInterface.php
+++ attendance-system-api/app/Http/Repositories/User/UserRepositoryInterface.php
@@ -1,5 +1,5 @@
 <?php
-namespace App\Repositories;
+namespace App\Repositories\User;
 
 interface UserRepositoryInterface
 {
