25920cd2c428450fc9561b7a2fbe9dd3d26964b3
Shau00n <flankeeeer7@gmail.com>
2023-04-26 00:33:10 +0900

    名前とテーブル名の変更（コントローラ名に統一）

--- attendance-system-api/database/migrations/2023_04_24_153810_create_attendances_table.php
+++ attendance-system-api/database/migrations/2023_04_24_153810_create_attendances_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('timecards', function (Blueprint $table) {
+        Schema::create('attendances', function (Blueprint $table) {
             $table->increments('id');
             $table->foreignId('user_id')->constrained('users');
             $table->dateTime('clock_in');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('timecards');
+        Schema::dropIfExists('attendances');
     }
 };
