59a996fdccfdaa2dcf136bf78ab1f6caa3d950d0
Shau00n <flankeeeer7@gmail.com>
2023-04-26 00:33:35 +0900

    Userリポジトリの編集

---
+++ attendance-system-api/app/Http/Repositories/User/DBUserRepository.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Repositories;
+
+use App\Models\User;
+
+class UserRepository implements UserRepositoryInterface
+{
+    protected $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function all()
+    {
+        return $this->user->all();
+    }
+
+    public function find($id)
+    {
+        return $this->user->findOrFail($id);
+    }
+
+    public function create(array $attributes)
+    {
+        return $this->user->create($attributes);
+    }
+
+    public function update($id, array $attributes)
+    {
+        $user = $this->user->findOrFail($id);
+        $user->update($attributes);
+
+        return $user;
+    }
+
+    public function delete($id)
+    {
+        $user = $this->user->findOrFail($id);
+        $user->delete();
+
+        return true;
+    }
+}
