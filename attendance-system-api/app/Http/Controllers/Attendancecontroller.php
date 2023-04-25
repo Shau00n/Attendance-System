83cf1ae41c0fca764e77a38f259d7e46929a9473
Shau00n <flankeeeer7@gmail.com>
2023-04-26 01:32:56 +0900

    ディレクトリを変更

--- attendance-system-api/app/Http/Controllers/Attendancecontroller.php
+++ attendance-system-api/app/Http/Controllers/Attendancecontroller.php
@@ -3,8 +3,80 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Repositories\Attendance\AttendanceRepositoryInterface;
+use Illuminate\Support\Facades\Validator;
 
 class AttendanceController extends Controller
 {
-    //
+    protected $attendanceRepository;
+
+    public function __construct(AttendanceRepositoryInterface $attendanceRepository)
+    {
+        $this->attendanceRepository = $attendanceRepository;
+    }
+
+    // 一覧表示機能
+    public function index()
+    {
+        $attendances = $this->attendanceRepository->getAll();
+        return response()->json($attendances);
+    }
+
+    // 単一データ表示機能
+    public function show($id)
+    {
+        $attendance = $this->attendanceRepository->findById($id);
+        return response()->json($attendance);
+    }
+
+    // データ作成機能
+    public function store(Request $request)
+    {
+        // バリデーションルールの定義
+        $rules = [
+            'user_id' => 'required|integer|exists:users,id',
+            'date' => 'required|date',
+            'status' => 'required|in:present,absent,late'
+        ];
+
+        // バリデーション
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $attendance = $this->attendanceRepository->create($request->all());
+        return response()->json($attendance, 201);
+    }
+
+    // データ更新機能
+    public function update(Request $request, $id)
+    {
+        $attendance = $this->attendanceRepository->findById($id);
+
+        // バリデーションルールの定義
+        $rules = [
+            'user_id' => 'required|integer|exists:users,id',
+            'date' => 'required|date',
+            'status' => 'required|in:present,absent,late'
+        ];
+
+        // バリデーション
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $attendance = $this->attendanceRepository->update($id, $request->all());
+        return response()->json($attendance, 200);
+    }
+
+    // データ削除機能
+    public function destroy($id)
+    {
+        $this->attendanceRepository->delete($id);
+        return response()->json(null, 204);
+    }
 }
+
+
--- attendance-system-api/app/Http/Repositories/Attendance/AttendanceRepositoryInterface.php
+++ attendance-system-api/app/Http/Repositories/Attendance/AttendanceRepositoryInterface.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Repositories;
+namespace App\Repositories\Attendance;
 
 interface AttendanceRepositoryInterface
 {
