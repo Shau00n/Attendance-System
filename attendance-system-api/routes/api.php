d36551996cb80f0cb7e6ebf163426a3f6365aba7
Shau00n <flankeeeer7@gmail.com>
2023-05-23 22:43:54 +0900

    エンドポイントの訂正を行った

--- attendance-system-api/routes/api.php
+++ attendance-system-api/routes/api.php
@@ -17,61 +17,52 @@ use App\Http\Controllers\CalendarController;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-
-// // User関連のエンドポイント
-// Route::get('/users', [UserController::class, 'index']);
-// Route::get('/users/{id}', [UserController::class, 'show']);
-// Route::post('/users', [UserController::class, 'store']);
-// Route::put('/users/{id}', [UserController::class, 'update']);
-// Route::delete('/users/{id}', [UserController::class, 'destroy']);
-
-// // Attendance関連のエンドポイント
-// Route::get('/attendances', [AttendanceController::class, 'index']);
-// Route::get('/attendances/{id}', [AttendanceController::class, 'show']);
-// Route::post('/attendances', [AttendanceController::class, 'store']);
-// Route::put('/attendances/{id}', [AttendanceController::class, 'update']);
-// Route::delete('/attendances/{id}', [AttendanceController::class, 'destroy']);
-
-// // Calendar関連のエンドポイント
-// Route::get('/calendars', [CalendarController::class, 'index']);
-// Route::get('/calendars/{id}', [CalendarController::class, 'show']);
-// Route::post('/calendars', [CalendarController::class, 'store']);
-// Route::put('/calendars/{id}', [CalendarController::class, 'update']);
-// Route::delete('/calendars/{id}', [CalendarController::class, 'destroy']);
+// api.php
 // ユーザー登録 
-Route::post('/register', [AuthController::class, 'register']); 
+Route::post('/register', [AuthController::class, 'register']);
 // ログイン 
-Route::post('/login', [AuthController::class, 'login']); 
+Route::post('/login', [AuthController::class, 'login']);
 // ログアウト 
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
 // User関連のエンドポイント（認証が必要） 
-Route::middleware('auth:sanctum')->group(function () { 
-    Route::get('/users', [UserController::class, 'index']); 
-    Route::get('/users/{id}', [UserController::class, 'show']); 
-    Route::post('/users', [UserController::class, 'store']); 
-    Route::put('/users/{id}', [UserController::class, 'update']); 
-    Route::delete('/users/{id}', [UserController::class, 'destroy']); 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('users', UserController::class);
 });
 
 // Attendance関連のエンドポイント（認証が必要） 
-Route::middleware('auth:sanctum')->group(function () { 
-    Route::get('/attendances', [AttendanceController::class, 'index']); 
-    Route::get('/attendances/{id}', [AttendanceController::class, 'show']); 
-    Route::post('/attendances', [AttendanceController::class, 'store']); 
-    Route::put('/attendances/{id}', [AttendanceController::class, 'update']); 
-    Route::delete('/attendances/{id}', [AttendanceController::class, 'destroy']); 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('attendances', AttendanceController::class);
 });
 
 // Calendar関連のエンドポイント（認証が必要） 
-Route::middleware('auth:sanctum')->group(function () { 
-    Route::get('/calendars', [CalendarController::class, 'index']); 
-    Route::get('/calendars/{id}', [CalendarController::class, 'show']); 
-    Route::post('/calendars', [CalendarController::class, 'store']); 
-    Route::put('/calendars/{id}', [CalendarController::class, 'update']); 
-    Route::delete('/calendars/{id}', [CalendarController::class, 'destroy']); 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('calendars', CalendarController::class);
 });
+
+// // User関連のエンドポイント（認証が必要） 
+// Route::middleware('auth:sanctum')->group(function () { 
+//     Route::get('/users', [UserController::class, 'index']); 
+//     Route::get('/users/{id}', [UserController::class, 'show']); 
+//     Route::post('/users', [UserController::class, 'store']); 
+//     Route::put('/users/{id}', [UserController::class, 'update']); 
+//     Route::delete('/users/{id}', [UserController::class, 'destroy']); 
+// });
+
+// // Attendance関連のエンドポイント（認証が必要） 
+// Route::middleware('auth:sanctum')->group(function () { 
+//     Route::get('/attendances', [AttendanceController::class, 'index']); 
+//     Route::get('/attendances/{id}', [AttendanceController::class, 'show']); 
+//     Route::post('/attendances', [AttendanceController::class, 'store']); 
+//     Route::put('/attendances/{id}', [AttendanceController::class, 'update']); 
+//     Route::delete('/attendances/{id}', [AttendanceController::class, 'destroy']); 
+// });
+
+// // Calendar関連のエンドポイント（認証が必要） 
+// Route::middleware('auth:sanctum')->group(function () { 
+//     Route::get('/calendars', [CalendarController::class, 'index']); 
+//     Route::get('/calendars/{id}', [CalendarController::class, 'show']); 
+//     Route::post('/calendars', [CalendarController::class, 'store']); 
+//     Route::put('/calendars/{id}', [CalendarController::class, 'update']); 
+//     Route::delete('/calendars/{id}', [CalendarController::class, 'destroy']); 
+// });
