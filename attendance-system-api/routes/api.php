6a7c47f9ddfb90ce420e1282dd21088054d7514d
Shau00n <flankeeeer7@gmail.com>
2023-05-23 21:15:12 +0900

    認証システム導入

--- attendance-system-api/routes/api.php
+++ attendance-system-api/routes/api.php
@@ -17,28 +17,61 @@ use App\Http\Controllers\CalendarController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+// // User関連のエンドポイント
+// Route::get('/users', [UserController::class, 'index']);
+// Route::get('/users/{id}', [UserController::class, 'show']);
+// Route::post('/users', [UserController::class, 'store']);
+// Route::put('/users/{id}', [UserController::class, 'update']);
+// Route::delete('/users/{id}', [UserController::class, 'destroy']);
+
+// // Attendance関連のエンドポイント
+// Route::get('/attendances', [AttendanceController::class, 'index']);
+// Route::get('/attendances/{id}', [AttendanceController::class, 'show']);
+// Route::post('/attendances', [AttendanceController::class, 'store']);
+// Route::put('/attendances/{id}', [AttendanceController::class, 'update']);
+// Route::delete('/attendances/{id}', [AttendanceController::class, 'destroy']);
+
+// // Calendar関連のエンドポイント
+// Route::get('/calendars', [CalendarController::class, 'index']);
+// Route::get('/calendars/{id}', [CalendarController::class, 'show']);
+// Route::post('/calendars', [CalendarController::class, 'store']);
+// Route::put('/calendars/{id}', [CalendarController::class, 'update']);
+// Route::delete('/calendars/{id}', [CalendarController::class, 'destroy']);
+// ユーザー登録 
+Route::post('/register', [AuthController::class, 'register']); 
+// ログイン 
+Route::post('/login', [AuthController::class, 'login']); 
+// ログアウト 
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+
+// User関連のエンドポイント（認証が必要） 
+Route::middleware('auth:sanctum')->group(function () { 
+    Route::get('/users', [UserController::class, 'index']); 
+    Route::get('/users/{id}', [UserController::class, 'show']); 
+    Route::post('/users', [UserController::class, 'store']); 
+    Route::put('/users/{id}', [UserController::class, 'update']); 
+    Route::delete('/users/{id}', [UserController::class, 'destroy']); 
 });
 
+// Attendance関連のエンドポイント（認証が必要） 
+Route::middleware('auth:sanctum')->group(function () { 
+    Route::get('/attendances', [AttendanceController::class, 'index']); 
+    Route::get('/attendances/{id}', [AttendanceController::class, 'show']); 
+    Route::post('/attendances', [AttendanceController::class, 'store']); 
+    Route::put('/attendances/{id}', [AttendanceController::class, 'update']); 
+    Route::delete('/attendances/{id}', [AttendanceController::class, 'destroy']); 
+});
 
-// User関連のエンドポイント
-Route::get('/users', [UserController::class, 'index']);
-Route::get('/users/{id}', [UserController::class, 'show']);
-Route::post('/users', [UserController::class, 'store']);
-Route::put('/users/{id}', [UserController::class, 'update']);
-Route::delete('/users/{id}', [UserController::class, 'destroy']);
-
-// Attendance関連のエンドポイント
-Route::get('/attendances', [AttendanceController::class, 'index']);
-Route::get('/attendances/{id}', [AttendanceController::class, 'show']);
-Route::post('/attendances', [AttendanceController::class, 'store']);
-Route::put('/attendances/{id}', [AttendanceController::class, 'update']);
-Route::delete('/attendances/{id}', [AttendanceController::class, 'destroy']);
-
-// Calendar関連のエンドポイント
-Route::get('/calendars', [CalendarController::class, 'index']);
-Route::get('/calendars/{id}', [CalendarController::class, 'show']);
-Route::post('/calendars', [CalendarController::class, 'store']);
-Route::put('/calendars/{id}', [CalendarController::class, 'update']);
-Route::delete('/calendars/{id}', [CalendarController::class, 'destroy']);
+// Calendar関連のエンドポイント（認証が必要） 
+Route::middleware('auth:sanctum')->group(function () { 
+    Route::get('/calendars', [CalendarController::class, 'index']); 
+    Route::get('/calendars/{id}', [CalendarController::class, 'show']); 
+    Route::post('/calendars', [CalendarController::class, 'store']); 
+    Route::put('/calendars/{id}', [CalendarController::class, 'update']); 
+    Route::delete('/calendars/{id}', [CalendarController::class, 'destroy']); 
+});
